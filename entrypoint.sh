cafd6551f45809075426397454810d91862c9722
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-07-01 13:57:03 +0200

    larger timeout

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,7 +47,7 @@ if [ "$WORKER_MODE" == "interface" ]; then
 #resttimesystem.sh > /host_var/log/resttimesystem.log 2>&1
     while true; do
         echo "interface worker starting"
-        DISPLAY="" gunicorn --log-level debug -b 0.0.0.0:8000 ddaworker.service:app 2>&1 
+        DISPLAY="" gunicorn --timeout 600  --log-level debug -b 0.0.0.0:8000 ddaworker.service:app 2>&1 
         echo "worker dead: restarting"
         sleep 1
     done | tee -a /var/log/containers/${CONTAINER_NAME}
